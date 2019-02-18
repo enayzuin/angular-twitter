bce9666fe9a892275e2f0e09afb347ea0fb53197
Zuin Bispo <enay.zuin.bispo@accenture.com>
2019-02-18 12:25:35 -0300

    ajustes server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
