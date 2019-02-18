dcb7a5f9b950d4a615f3e0b475dff0a3929db38e
Zuin Bispo <enay.zuin.bispo@accenture.com>
2019-02-18 13:47:54 -0300

    ng build antes de deploy

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/'));
+
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
